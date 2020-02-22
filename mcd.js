857bf7f96421f9ff922db1ed1a94072d96d04406
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-02-22 22:27:09 +0100

    Update mcd.js

--- mcd.js
+++ mcd.js
@@ -1,3 +1,28 @@
 /**
 * Scrivere qui il codice delle funzioni mcd(x, y) e mcdRecursive(x, y)
 **/
+const x = 1256
+const y = 336
+function mcdRecursive(a, b) {
+  let r = a % b
+  if ( r === 0) {
+    return b
+  } else{
+    a = b
+    b = r
+  }
+  return mcdRecursive(a,b)
+}
+const exe = mcdRecursive(x,y)
+
+
+
+
+function mcdIterative(a, b) {
+  while (b !== 0) {
+     let r = a % b
+     a = b
+     b = r
+  }
+  return a;
+}
