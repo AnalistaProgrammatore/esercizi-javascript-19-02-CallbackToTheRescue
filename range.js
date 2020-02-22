00036cac1bed52eb00bba4fe19a6cad813aab1f8
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-02-22 22:44:43 +0100

    Update range.js

--- range.js
+++ range.js
@@ -1,3 +1,26 @@
 /**
 * Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
 **/
+
+//al momento non sono riuscito a trovare un modo di farla funzionare senza dichiarare anche la variabile x
+function range(a, b, x) {
+  let arr = []
+  if (x < 0) {
+    for (let i = a; i >= b; i += x){
+      arr.push(i)
+    }
+  } else {
+    for (let i = a; i <= b; i += x){
+      arr.push(i)
+    }
+  }
+  return arr
+}
+
+function sum(arr){
+  let acc= 0
+  for(let i=0; i< arr.length; i++){
+    acc += arr[i]
+  }
+  return acc
+}
