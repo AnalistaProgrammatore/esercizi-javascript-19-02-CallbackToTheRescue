ca1ae5b80d09bcbec5531cae75cf1df1413584d5
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-02-22 22:48:45 +0100

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -2,3 +2,13 @@
 * Scrivere qui il codice dell'esercizio 2 del capitolo 4 di eloquentJS
 * Vi ricordo che le funzioni DEVONO ESSERE PURE e implementate una in modo IMPERATIVO e una in modo DICHIARATIVO
 **/
+function reverseImperative(arr) {
+  let arrCopied = []
+  for (let i = arr.length; i > 0; i--) {
+    arrCopied.push(arr[i-1])
+  }
+ return arrCopied
+}
+
+
+const reverseDeclarative = arr => arr.reduce((a, b) => [b, ...a], [])
