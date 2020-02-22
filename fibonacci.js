718d37f505d48d3ece08466e12c47e8c22806026
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-02-22 22:24:45 +0100

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,13 @@
 /**
 * Scrivere qui il codice della funzione fibonacci(n)
 **/
+
+const number = 30
+const fibonacciRecursive = function(num) {
+    if(num < 2){
+      return num
+ }else{
+      return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
+  }
+}
+const exe = fibonacciRecursive(number)
