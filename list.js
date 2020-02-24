98d0730f0c1c8daab955d4446c83b4a45fb847ae
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-02-24 23:21:03 +0100

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,11 @@
 /**
 * Scrivere qui il codice dell'esercizio 3 del capitolo 4 di eloquentJS
 **/
+function arrayToList(arr) {
+    let nested = null
+    for (let i = 0; i < arr.length; i++) {
+        nested = { value: arr[i], rest: nested }
+    }
+    return nested
+}
+
